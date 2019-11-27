c12f49bfaa9cd17ba87b3898e8bcfdec3d98522e
Argenis De La Rosa <hennyrosexo@gmail.com>
2019-11-27 00:55:36 -0500

    created a node array to hold the scnnodes from my dice array of objects

--- AR Dice/ViewController.swift
+++ AR Dice/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     @IBOutlet var sceneView: ARSCNView!
     
+    // Create a new scene
+    let scene = SCNScene(named: "art.scnassets/ship.scn")!
+    
+    var diceArray = [SCNNode]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,6 +26,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the view's delegate
         sceneView.delegate = self
+        sceneView.autoenablesDefaultLighting = true
         //
         //        let cube = SCNSphere(radius: 0.2)
         //        let material = SCNMaterial()
@@ -33,54 +39,81 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         //        node.geometry = cube
         
         //        sceneView.scene.rootNode.addChildNode(node)
-        sceneView.autoenablesDefaultLighting = true
+        
         
         
     }
     
-    
-    //        // Create a new scene
-    //        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-    //        
-    //        // Set the scene to the view
-    //        sceneView.scene = scene
-}
-
-override func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
-    
-    // Create a session configuration
-    let configuration = ARWorldTrackingConfiguration()
-    
-    configuration.planeDetection = .horizontal
-    
-    // Run the view's session
-    sceneView.session.run(configuration)
-}
-
-override func viewWillDisappear(_ animated: Bool) {
-    super.viewWillDisappear(animated)
-    
-    // Pause the view's session
-    sceneView.session.pause()
-}
-
-// MARK: - Delegates
-
-override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    if let touch = touches.first {
-        let touchLocation = touch.location(in: sceneView)
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
-        let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
         
-        if let hitResults = results.first {
+        configuration.planeDetection = .horizontal
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    // MARK: - Functions
+    
+    func roll(dice: SCNNode) {
+        
+        let randomX = Float(arc4random_uniform(4) + 1) * (Float.pi/2)
+        let randomZ = Float(arc4random_uniform(4) + 1) * (Float.pi/2)
+        
+        diceNode.runAction(SCNAction.rotateBy(
+            x: CGFloat(randomX),
+            y: 0,
+            z: CGFloat(randomZ),
+            duration: 0.5))
+    }
+    
+    func rollAll() {
+        
+        if !diceArray.isEmpty {
+            for dice in diceArray {
+                roll(dice)
+            }
+        }
+    }
+      
+    // MARK: - Delegates
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if let touch = touches.first {
             
-            let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+            let touchLocation = touch.location(in: sceneView)
             
-            if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
-                diceNode.position = SCNVector3(0, 0, -0.1)
+            let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+            
+            if let hitResults = results.first {
                 
-                sceneView.scene.rootNode.addChildNode(diceNode)
+                let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+                
+                if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+                    
+                    diceNode.position = SCNVector3(
+                        x: hitResults.worldTransform.columns.3.x,
+                        y: hitResults.worldTransform.columns.3.y + diceNode.boundingSphere.radius,
+                        z: hitResults.worldTransform.columns.3.z
+                        
+                    )
+                    
+                    diceArray.append(diceNode)
+                    
+                    sceneView.scene.rootNode.addChildNode(diceNode)
+
+                }
             }
         }
     }
