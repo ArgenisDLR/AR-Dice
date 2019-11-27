bae01c5e9507e3f709ce6d921dc317fbc5fb6ac6
Argenis De La Rosa <hennyrosexo@gmail.com>
2019-11-26 23:29:59 -0500

    refactored the ui

--- AR Dice/ViewController.swift
+++ AR Dice/ViewController.swift
@@ -11,7 +11,7 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
@@ -21,53 +21,68 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the view's delegate
         sceneView.delegate = self
-//
-//        let cube = SCNSphere(radius: 0.2)
-//        let material = SCNMaterial()
-//
-//        material.diffuse.contents = UIImage(named: "art.scnassets/moon.jpg")
-//        cube.materials = [material]
-//
-//        let node = SCNNode()
-//        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
-//        node.geometry = cube
-
-//        sceneView.scene.rootNode.addChildNode(node)
+        //
+        //        let cube = SCNSphere(radius: 0.2)
+        //        let material = SCNMaterial()
+        //
+        //        material.diffuse.contents = UIImage(named: "art.scnassets/moon.jpg")
+        //        cube.materials = [material]
+        //
+        //        let node = SCNNode()
+        //        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
+        //        node.geometry = cube
+        
+        //        sceneView.scene.rootNode.addChildNode(node)
         sceneView.autoenablesDefaultLighting = true
         
-        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
         
-        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
-            diceNode.position = SCNVector3(0, 0, -0.1)
+    }
+    
+    
+    //        // Create a new scene
+    //        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+    //        
+    //        // Set the scene to the view
+    //        sceneView.scene = scene
+}
+
+override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    
+    // Create a session configuration
+    let configuration = ARWorldTrackingConfiguration()
+    
+    configuration.planeDetection = .horizontal
+    
+    // Run the view's session
+    sceneView.session.run(configuration)
+}
+
+override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    
+    // Pause the view's session
+    sceneView.session.pause()
+}
+
+// MARK: - Delegates
+
+override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    if let touch = touches.first {
+        let touchLocation = touch.location(in: sceneView)
+        
+        let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+        
+        if let hitResults = results.first {
             
-            sceneView.scene.rootNode.addChildNode(diceNode)
+            let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+            
+            if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+                diceNode.position = SCNVector3(0, 0, -0.1)
+                
+                sceneView.scene.rootNode.addChildNode(diceNode)
+            }
         }
-
-        
-//        // Create a new scene
-//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-//        
-//        // Set the scene to the view
-//        sceneView.scene = scene
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-        
-        configuration.planeDetection = .horizontal
-
-        // Run the view's session
-        sceneView.session.run(configuration)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        // Pause the view's session
-        sceneView.session.pause()
     }
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
@@ -89,5 +104,4 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             return
         }
     }
-
 }
